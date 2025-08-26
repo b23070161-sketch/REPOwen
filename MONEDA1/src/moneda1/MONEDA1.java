f94b49897a46aa38653b2a004e46b372a3812a73
owenh <b23070161@itcelaya.edu.mx>
2025-08-26 00:57:52 -0600

    Primer Snapshot

---
+++ MONEDA1/src/moneda1/MONEDA1.java
@@ -0,0 +1,31 @@
+package moneda1;
+import java.util.Scanner;
+
+public class MONEDA1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] denominaciones = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+
+        System.out.println("Bienvenido al sistema de cambios en eurOS, introduce una cantidad de dinero y el programa te dara el cambio que deberas dar.");
+        System.out.print("CANTIDAD: ");
+
+        while (true) {
+            int cantidad = sc.nextInt();
+            if (cantidad == 0) break;
+
+            System.out.println(cantidad);
+
+            for (int i = 0; i < denominaciones.length; i++) {
+                if (cantidad >= denominaciones[i]) {
+                    int num = cantidad / denominaciones[i];
+                    System.out.println(num + "de " + denominaciones[i]);
+                    cantidad = cantidad % denominaciones[i];
+                }
+            }
+            System.out.print("CANTIDAD: ");
+        }
+
+        sc.close();
+    }
+}
